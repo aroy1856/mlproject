c53c70a6067f3a3a3657c1b1f4d3a539a3816d7a
Abhishek Roy <abhishekroy@Abhisheks-MacBook-Pro.local>
2025-06-14 15:45:09 +0530

    updating data ingestion file

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,6 +6,7 @@ from dataclasses import dataclass
 
 from src.logger import logging
 from src.exception import CustomException
+from src.components.data_transformation import DataTransformation
 
 @dataclass
 class DataIngestionConfig:
@@ -42,4 +43,7 @@ class DataIngestion:
 
 if __name__ == "__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data_path,test_data_path = obj.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data_path,test_data_path)
